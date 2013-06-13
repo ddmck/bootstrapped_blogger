5c1e503cf07b20e7264dad067b92b68f84460765
ddmck <ddmckendrick@gmail.com>
2013-06-13 22:55:49 +0100

    Add index and show actions

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger2::Application.config.secret_token = 'cb1cb11196d1dd542846d98b5e11dce6cfcccffa6215e06c2e03382765b9a92d3c57eb8146818a9e83c74e0613d62a613e086177f53d7c8cece578e53e8ef758'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Blogger2::Application.routes.draw do
+
+  resources :articles
+
+end
