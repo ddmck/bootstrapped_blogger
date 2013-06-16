40a8dc1e4cec4f0f085a9fbe50f978a6575935c1
ddmck <ddmckendrick@gmail.com>
2013-06-17 00:37:47 +0100

    Add styling to index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,8 @@ class Article < ActiveRecord::Base
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  
+  has_attached_file :image, styles: { medium: "300x200>", thumb: "100x66>" }
+
   def tag_list
     tags.join(", ")
   end
